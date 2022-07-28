274bcff965f75541373deac94ab999b23c3aba13
Takumi Koide <cmj14059@ict.nitech.ac.jp>
2022-07-28 18:37:08 +0900

    remove comma

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_
 
 im = imread(img_url)
 
-st.image(im, caption='changed this text here', , , , , , , , , ,
+st.image(im, caption='changed this text here',
          use_column_width=True)
 
 
